8de89f31cad83134a340fc9b346fac6ca4bd2960
Orren <ochapman@walmart.com>
2013-03-10 15:52:59 -0700

    Filtering!!

--- js/tvFinder.js
+++ js/tvFinder.js
@@ -1,2 +1,29 @@
-$('.dropdown-toggle').dropdown();
-$('#tvFinderOfferContainerId').deferredImage();
+$('.dropdown-toggle')
+    .dropdown()
+    .parent()
+    .find(".dropdown-menu")
+    .find("a")
+    .click(function () {
+        var $this = $(this)
+            , value = $this.text()
+            , $parent = $this.parents("ul.dropdown-menu")
+            , role = $parent.attr("data-role")
+            ;
+        if (role === 'filter') {
+            $('#tvFinderOfferContainerId').isotope({ filter: '.tvFinderOffer[data-' + $parent.attr("data-attr") + '=' + value + ']' }, function ($items) {
+                var id = this.attr('id'),
+                    len = $items.length;
+                console.log('Isotope has filtered for ' + len + ' items in #' + id);
+            });
+        }
+        return false;
+    })
+;
+
+$('#tvFinderOfferContainerId')
+    .isotope({
+        itemSelector: '.tvFinderOffer',
+        layoutMode: 'fitRows'
+    })
+    .deferredImage()
+;
